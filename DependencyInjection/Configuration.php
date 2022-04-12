c4814087edee00022bed6460154567ca1fb374bd
joshugroupdev <101131887+joshugroupdev@users.noreply.github.com>
2022-04-12 14:38:05 -0400

    Update treeBuilder initialization

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,8 +24,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('twilio');
+        $treeBuilder = new TreeBuilder('twilio');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
